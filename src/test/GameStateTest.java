586b8da8009f761a5a52a8eda4c9d7bb1eb567a2
Thomas Francis <tfrancis@scu.edu>
2019-03-21 15:54:52 -0700

    Adding more features to Game

--- src/test/GameStateTest.java
+++ src/test/GameStateTest.java
@@ -1,10 +1,15 @@
 package test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import Game.Game;
 import Game.GameState;
+import Game.Physics;
+import Projectiles.Bullet;
 
 public class GameStateTest {
 
@@ -33,6 +38,45 @@ public class GameStateTest {
     gameState.startGame();
     gameState.pauseGame();
     assertTrue(gameState.getState() == GameState.State.PAUSED);
+  }
+  
+  @Test
+  public void basicDifficultyTest() {
+    gameState.startGame();
+    gameState.beginnerDifficulty();
+    assertThat(gameState.getShip().getHealth(), is(100));
+    assertThat(gameState.getShip().getCollisionDamage(), is(5));
+    assertThat(gameState.getShip().getLevelIncreaseAmmo(), is(50));
+    assertThat(Physics.randomDyMax, is(3));
+    assertThat(Physics.randomDyMin, is(1));
+    assertThat(Physics.difficultyMult, is(5));
+    assertThat(gameState.getDifficulty(), is(100));
+  }
+  
+  @Test
+  public void aceDifficultyTest() {
+    gameState.startGame();
+    gameState.regularDifficulty();
+    assertThat(gameState.getShip().getHealth(), is(100));
+    assertThat(gameState.getShip().getCollisionDamage(), is(10));
+    assertThat(gameState.getShip().getLevelIncreaseAmmo(), is(25));
+    assertThat(Physics.randomDyMax, is(4));
+    assertThat(Physics.randomDyMin, is(1));
+    assertThat(Physics.difficultyMult, is(10));
+    assertThat(gameState.getDifficulty(), is(75));
+  }
+  
+  @Test
+  public void proDifficultyTest() {
+    gameState.startGame();
+    gameState.proDifficulty();
+    assertThat(gameState.getShip().getHealth(), is(100));
+    assertThat(gameState.getShip().getCollisionDamage(), is(25));
+    assertThat(gameState.getShip().getLevelIncreaseAmmo(), is(10));
+    assertThat(Physics.randomDyMax, is(6));
+    assertThat(Physics.randomDyMin, is(2));
+    assertThat(Physics.difficultyMult, is(15));
+    assertThat(gameState.getDifficulty(), is(50));
   }
 
 }
